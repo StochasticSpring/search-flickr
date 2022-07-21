1a357851367c3f23827dfbe12cb26aad68adfaeb
Eugene Lai <ejt.lai@gmail.com>
2022-07-21 12:17:14 +0930

    Implements the debounced query to Flickr

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,38 @@
-import Head from 'next/head'
-import Header from '@components/Header'
-import Footer from '@components/Footer'
+import Head from "next/head";
+import Header from "@components/Header";
+import Footer from "@components/Footer";
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TextField from "@mui/material/TextField";
+import useDebouncedValue from "../hooks/useDebouncedValue";
 
 const QUERY_DEBOUNCE_MS = 300;
 
 export default function Home() {
   const [userInput, setUserInput] = useState("");
+  const [queryResultItems, setQueryResultItems] = useState([]);
+  const debouncedUserInput = useDebouncedValue(userInput, QUERY_DEBOUNCE_MS);
+
+  const fetchQueryResults = async (input) => {
+    const response = await fetch(
+      `/flickr/services/feeds/photos_public.gne?format=json&tags=${input}`
+    );
+    const bodyText = await response.text();
+
+    // Flickr returns a JSON wrapped in some extra text
+    const jsonStr = bodyText.replace("jsonFlickrFeed(", "").slice(0, -1);
+    const items = JSON.parse(jsonStr).items
+    setQueryResultItems(items);
+    console.log(items);
+  };
+
+  useEffect(
+    () => {
+      if (debouncedUserInput.trim().length > 0) {
+        fetchQueryResults(debouncedUserInput.trim());
+      }
+    }, [debouncedUserInput]
+  )
 
   return (
     <div className="container">
@@ -25,11 +49,11 @@ export default function Home() {
           placeholder="Search images by tag..."
           onChange={(e) => setUserInput(e.target.value)}
           value={userInput}
-          sx={{maxWidth: "600px"}}
+          sx={{ maxWidth: "600px" }}
         />
       </main>
 
       <Footer />
     </div>
-  )
+  );
 }
