ef79b624425f8287a7a301d125b853c53d9d6bb7
Eugene Lai <ejt.lai@gmail.com>
2022-07-22 11:57:25 +0930

    Adds some transitions to make expansion less jarring

--- components/ListItemWithExpansion.jsx
+++ components/ListItemWithExpansion.jsx
@@ -1,40 +1,50 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { Typography } from "@mui/material";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
 import ImageWithSkeleton from "@components/ImageWIthSkeleton";
-import styled, { ThemeProvider } from 'styled-components';
+import Slide from "@mui/material/Slide";
+import Fade from "@mui/material/Fade";
 
 const ImageListItemWithExpansion = (props) => {
   const { item, ...otherProps } = props;
   const [isExpanded, setIsExpanded] = useState(false);
+  const ImageListItemRef = useRef(null);
 
   return (
     <ImageListItem
       onMouseEnter={() => setIsExpanded(true)}
       onMouseLeave={() => setIsExpanded(false)}
+      ref={ImageListItemRef}
       {...otherProps}
     >
       <a href={item.link} target="_blank" rel="noopener noreferrer">
         <ImageWithSkeleton src={item.media.m} alt={item.title} loading="lazy" />
       </a>
-      <ImageListItemBar
-        title={item.title}
-        subtitle={
-          isExpanded ? (
+      <Slide
+        direction="up"
+        in={isExpanded}
+        container={ImageListItemRef.current}
+      >
+        <ImageListItemBar
+          title={item.title}
+          subtitle={
             <>
-              <Typography>{`by: ${item.author}`}</Typography>
-              <Typography>{`on: ${item.date_taken.split("T")[0]}`}</Typography>
+              <Typography variant="inherit">{`by: ${item.author}`}</Typography>
+              <Typography variant="inherit">{`on: ${
+                item.date_taken.split("T")[0]
+              }`}</Typography>
+              <Typography
+                variant="inherit"
+                sx={{ whiteSpace: "normal" }}
+              >{`tags: ${item.tags}`}</Typography>
             </>
-          ) : (
-            item.author
-          )
-        }
-        sx={{
-          height: isExpanded ? "50%" : "default",
-          transition: "all 4s 1s",
-        }}
-      />
+          }
+        />
+      </Slide>
+      <Fade in={!isExpanded}>
+        <ImageListItemBar title={item.title} subtitle={item.author} />
+      </Fade>
     </ImageListItem>
   );
 };
